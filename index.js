68e67acc162186ea43ab7da6915ce793c45b1025
gladosislove <33029439+gladosislove@users.noreply.github.com>
2018-01-20 13:15:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -47,13 +47,6 @@ bot.on('ready', () => {
   }
   });
 
-  bot.on('message', function() {
-    if (message.content.includes(("$loop")) ) {
-      var interval = setInterval (function () {
-        message.channel.send("chat-with-bots", "Hey! Welcome to #chat-with-bots! Here, you can ask any of us bots to do things without clogging up #general. Each bot has their own prefix and help command, myself included. So, converse away!")
-      }, 86400000); 
-    }
-});
 
   //list of commands
   const commands = ["```!hi -- greets the user", "!ping -- a nice game of table tennis", 
