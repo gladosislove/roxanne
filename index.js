280ef39a2264554436cd10d5569b50202f47d229
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-01-27 20:35:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -221,7 +221,7 @@ bot.on('message', async message => {
         
         bot.channels.get('530728851194642442').send({embed});
 
-        message.channel.send(`${member.user.tag} has been kicked by @${message.author.tag} for ${reason}.`)
+        message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag} for ${reason}.`)
 
     }
 
