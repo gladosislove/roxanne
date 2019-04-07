13f085d1ef026ecbf269627528f22a46424453ef
gladosislove <33029439+gladosislove@users.noreply.github.com>
2019-04-07 15:15:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ bot.on('message', async message => {
     if (message.content === PREFIX + 'boop') {
         if(message.mentions.members.size == 1) {
             message.channel.send("Psst. Happy one month anniversary, Tom! ; v ;", {
-                file: "./tom but he's even more uwu.png"
+                file: ["./tom but he's even more uwu.png"]
             });
         } else {
             message.reply('please mention a user.')
